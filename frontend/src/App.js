5760e230873500b23aaaef7bac7c2093a6975a43
Adharsh-NJ <74034697+Adharsh-NJ@users.noreply.github.com>
2021-06-06 19:47:42 +0530

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,38 @@
+
+import { useState,useEffect } from 'react';
+import './App.css';
+import Video from './component/Video'
+import axios from './axios'
+function App() {
+  const [videos,setVideos] =useState([])
+
+  useEffect(() => {
+    const fetchData =async()=>{
+      const res=await axios.get("/v2/posts")
+      setVideos(res.data)
+    }
+    fetchData()
+  }, [])
+  return (
+    <div className="app">
+    
+    <div className="app__videos">
+    {videos.map(({_id,url,channel,description,song,likes,shares,messages})=>(
+      <Video 
+      key={_id}
+      url={url}
+      channel={channel}
+                description={description}
+                song={song}
+                likes={likes}
+                shares={shares}
+                messages={messages}
+    />
+    ))}
+    {console.log(videos)}
+     </div>
+    </div>
+  );
+}
+
+export default App;
